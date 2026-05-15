6ed6bd2b2700ff3c40eb13d1db85287b0c2f51a3
veltran11 <veltran11@gmail.com>
2026-05-15 10:00:44 -0300

    debug: cambiar title a VERSION 2025

--- public/index.php
+++ public/index.php
@@ -15,7 +15,7 @@ $cssVersion = file_exists($cssFile) ? filemtime($cssFile) : time();
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>FACTURación</title>
+  <title>FACTURación - VERSION 2025</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap" rel="stylesheet">
